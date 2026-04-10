6bf33c2e64dbe49847e2d98b89948c8fc444f946
estudianteUC <estudianteUC@UCUXB100109.continental.edu.pe>
2026-04-10 15:53:47 -0500

    wazaaaaaaaaaa

--- UNIDAD 1/Semana 4/index.js
+++ UNIDAD 1/Semana 4/index.js
@@ -2,13 +2,13 @@ const express = require("express");
 const axios = require("axios");
 const app = express();
 
-// Servir archivos estáticos desde la carpeta "public" [cite: 53]
+// Servir archivos estáticos desde la carpeta "public" 
 app.use(express.static("public"));
 
-// Ruta API para obtener usuarios [cite: 54, 55]
+// Ruta API para obtener usuarios 
 app.get("/api/usuarios", async (req, res) => {
     try {
-        // Petición GET a JSONPlaceholder [cite: 57]
+        // Petición GET a JSONPlaceholder 
         const respuesta = await axios.get("https://jsonplaceholder.typicode.com/users");
         res.json(respuesta.data); 
     } catch (error) {
@@ -16,7 +16,7 @@ app.get("/api/usuarios", async (req, res) => {
     }
 });
 
-// Iniciar servidor en el puerto 3000 [cite: 62, 64]
+// Iniciar servidor en el puerto 3000 
 app.listen(3000, () => {
     console.log("Servidor en http://localhost:3000");
 });
